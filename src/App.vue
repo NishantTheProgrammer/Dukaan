6f503239c421b24388a39f8c8d32957230f603fc
Nishant <knishant113@gmail.com>
2021-12-21 00:53:33 +0530

    added home page and Terms page and Header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
   <router-view/>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue"; // @ is an alias to /src
+export default defineComponent({
+  name: "Terms",
+  components: {
+    Header,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/views/Terms.vue
@@ -0,0 +1,446 @@
+<template>
+  <div class="breadcrumb">
+    <div class="container">
+      <div class="breadcrumb-inner">
+        <ul class="list-inline list-unstyled">
+          <li><a href="#">Home</a></li>
+          <li class="active">Terms & Conditions</li>
+        </ul>
+      </div>
+      <!-- /.breadcrumb-inner -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.breadcrumb -->
+
+  <div class="body-content">
+    <div class="container">
+      <div class="terms-conditions-page">
+        <div class="row">
+          <div class="col-md-12 terms-conditions">
+            <h2 class="heading-title">Terms and Conditions</h2>
+            <div class="">
+              <h3>Intellectual Propertly</h3>
+              <ol>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+              </ol>
+              <h3>Termination</h3>
+              <ol>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  quis diam erat. Duis velit lectus, posuere a blandit sit amet,
+                  tempor at lorem. Donec ultricies, lorem sed ultrices interdum.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+              </ol>
+              <h3>Changes to this agreement</h3>
+              <p>
+                We reserve the right, at our sole discretion, to modify or
+                replace these Terms and Conditions by posting the updated terms
+                on the Site. Your continued use of the Site after any such
+                changes constitutes your acceptance of the new Terms and
+                Conditions.
+              </p>
+              <h3>Contact Us</h3>
+              <p>
+                If you have any questions about this Agreement, please contact
+                us filling this
+                <a href="#" class="contact-form">contact form</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.sigin-in-->
+      <!-- ============================================== BRANDS CAROUSEL ============================================== -->
+      <div id="brands-carousel" class="logo-slider wow fadeInUp">
+        <div class="logo-slider-inner">
+          <div
+            id="brand-slider"
+            class="owl-carousel brand-slider custom-carousel owl-theme"
+          >
+            <div class="item m-t-15">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand1.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item m-t-10">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand2.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand3.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand4.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand5.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand6.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand2.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand4.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand1.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+
+            <div class="item">
+              <a href="#" class="image">
+                <img
+                  data-echo="assets/images/brands/brand5.png"
+                  src="assets/images/blank.gif"
+                  alt=""
+                />
+              </a>
+            </div>
+            <!--/.item-->
+          </div>
+          <!-- /.owl-carousel #logo-slider -->
+        </div>
+        <!-- /.logo-slider-inner -->
+      </div>
+      <!-- /.logo-slider -->
+      <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.body-content -->
+  <!-- ============================================================= FOOTER ============================================================= -->
+
+  <!-- ============================================== INFO BOXES ============================================== -->
+  <div class="row our-features-box">
+    <div class="container">
+      <ul>
+        <li>
+          <div class="feature-box">
+            <div class="icon-truck"></div>
+            <div class="content-blocks">We ship worldwide</div>
+          </div>
+        </li>
+        <li>
+          <div class="feature-box">
+            <div class="icon-support"></div>
+            <div class="content-blocks">call +1 800 789 0000</div>
+          </div>
+        </li>
+        <li>
+          <div class="feature-box">
+            <div class="icon-money"></div>
+            <div class="content-blocks">Money Back Guarantee</div>
+          </div>
+        </li>
+        <li>
+          <div class="feature-box">
+            <div class="icon-return"></div>
+            <div class="content">30 days return</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- /.info-boxes -->
+  <!-- ============================================== INFO BOXES : END ============================================== -->
+
+  <!-- ============================================================= FOOTER ============================================================= -->
+  <footer id="footer" class="footer color-bg">
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="address-block">
+              <!-- /.module-heading -->
+
+              <div class="module-body">
+                <ul class="toggle-footer" style="">
+                  <li class="media">
+                    <div class="pull-left">
+                      <span class="icon fa-stack fa-lg">
+                        <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <p>ThemesGround, 789 Main rd, Anytown, CA 12345 USA</p>
+                    </div>
+                  </li>
+                  <li class="media">
+                    <div class="pull-left">
+                      <span class="icon fa-stack fa-lg">
+                        <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <p>
+                        +(888) 123-4567<br />
+                        +(888) 456-7890
+                      </p>
+                    </div>
+                  </li>
+                  <li class="media">
+                    <div class="pull-left">
+                      <span class="icon fa-stack fa-lg">
+                        <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <span><a href="#">marazzo@themesground.com</a></span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- /.module-body -->
+          </div>
+          <!-- /.col -->
+
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="module-heading">
+              <h4 class="module-title">Customer Service</h4>
+            </div>
+            <!-- /.module-heading -->
+
+            <div class="module-body">
+              <ul class="list-unstyled">
+                <li class="first">
+                  <a href="#" title="Contact us">My Account</a>
+                </li>
+                <li><a href="#" title="About us">Order History</a></li>
+                <li><a href="#" title="faq">FAQ</a></li>
+                <li><a href="#" title="Popular Searches">Specials</a></li>
+                <li class="last">
+                  <a href="#" title="Where is my order?">Help Center</a>
+                </li>
+              </ul>
+            </div>
+            <!-- /.module-body -->
+          </div>
+          <!-- /.col -->
+
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="module-heading">
+              <h4 class="module-title">Corporation</h4>
+            </div>
+            <!-- /.module-heading -->
+
+            <div class="module-body">
+              <ul class="list-unstyled">
+                <li class="first">
+                  <a title="Your Account" href="#">About us</a>
+                </li>
+                <li>
+                  <a title="Information" href="#">Customer Service</a>
+                </li>
+                <li><a title="Addresses" href="#">Company</a></li>
+                <li>
+                  <a title="Addresses" href="#">Investor Relations</a>
+                </li>
+                <li class="last">
+                  <a title="Orders History" href="#">Advanced Search</a>
+                </li>
+              </ul>
+            </div>
+            <!-- /.module-body -->
+          </div>
+          <!-- /.col -->
+
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="module-heading">
+              <h4 class="module-title">Why Choose Us</h4>
+            </div>
+            <!-- /.module-heading -->
+
+            <div class="module-body">
+              <ul class="list-unstyled">
+                <li class="first">
+                  <a href="#" title="About us">Shopping Guide</a>
+                </li>
+                <li><a href="#" title="Blog">Blog</a></li>
+                <li><a href="#" title="Company">Company</a></li>
+                <li>
+                  <a href="#" title="Investor Relations">Investor Relations</a>
+                </li>
+                <li class="last">
+                  <a href="contact-us.html" title="Suppliers">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <!-- /.module-body -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright-bar">
+      <div class="container">
+        <div class="col-xs-12 col-sm-4 no-padding social">
+          <ul class="link">
+            <li class="fb pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="Facebook"></a>
+            </li>
+            <li class="tw pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="Twitter"></a>
+            </li>
+            <li class="googleplus pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="GooglePlus"></a>
+            </li>
+            <li class="rss pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="RSS"></a>
+            </li>
+            <li class="pintrest pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="PInterest"></a>
+            </li>
+            <li class="linkedin pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="Linkedin"></a>
+            </li>
+            <li class="youtube pull-left">
+              <a target="_blank" rel="nofollow" href="#" title="Youtube"></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 no-padding copyright">
+          <a target="_blank" href="https://www.templateshub.net"
+            >Templates Hub</a
+          >
+        </div>
+        <div class="col-xs-12 col-sm-4 no-padding">
+          <div class="clearfix payment-methods">
+            <ul>
+              <li><img src="assets/images/payments/1.png" alt="" /></li>
+              <li><img src="assets/images/payments/2.png" alt="" /></li>
+              <li><img src="assets/images/payments/3.png" alt="" /></li>
+              <li><img src="assets/images/payments/4.png" alt="" /></li>
+              <li><img src="assets/images/payments/5.png" alt="" /></li>
+            </ul>
+          </div>
+          <!-- /.payment-methods -->
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+

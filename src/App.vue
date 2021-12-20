86b8fa6be696a2a5544333adab69ddf46099fc8b
Nishant <knishant113@gmail.com>
2021-12-21 01:00:26 +0530

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
   <router-view/>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/Header.vue"; // @ is an alias to /src
+import Footer from "@/components/Footer.vue"; // @ is an alias to /src
 export default defineComponent({
   name: "Terms",
   components: {
     Header,
+    Footer,
   },
 });
 </script>

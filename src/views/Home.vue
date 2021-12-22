67c1c89a67aa2455393b8562fa4853da9860e83b
Nishant <knishant113@gmail.com>
2021-12-23 00:16:07 +0530

    broken product api fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -755,123 +755,6 @@
             </div>
           </div>
 
-          <div class="clearfix filters-container m-t-10">
-            <div class="row">
-              <div class="col col-sm-6 col-md-3 col-lg-3 col-xs-6">
-                <div class="filter-tabs">
-                  <ul
-                    id="filter-tabs"
-                    class="nav nav-tabs nav-tab-box nav-tab-fa-icon"
-                  >
-                    <li class="active">
-                      <a data-toggle="tab" href="#grid-container"
-                        ><i class="icon fa fa-th-large"></i>Grid</a
-                      >
-                    </li>
-                  </ul>
-                </div>
-                <!-- /.filter-tabs -->
-              </div>
-              <!-- /.col -->
-              <div class="col col-sm-12 col-md-5 col-lg-5 hidden-sm">
-                <div class="col col-sm-6 col-md-6 no-padding">
-                  <div class="lbl-cnt">
-                    <span class="lbl">Sort by</span>
-                    <div class="fld inline">
-                      <div
-                        class="
-                          dropdown dropdown-small dropdown-med dropdown-white
-                          inline
-                        "
-                      >
-                        <button
-                          data-toggle="dropdown"
-                          type="button"
-                          class="btn dropdown-toggle"
-                        >
-                          Position <span class="caret"></span>
-                        </button>
-                        <ul role="menu" class="dropdown-menu">
-                          <li role="presentation"><a href="#">position</a></li>
-                          <li role="presentation">
-                            <a href="#">Price:Lowest first</a>
-                          </li>
-                          <li role="presentation">
-                            <a href="#">Price:HIghest first</a>
-                          </li>
-                          <li role="presentation">
-                            <a href="#">Product Name:A to Z</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <!-- /.fld -->
-                  </div>
-                  <!-- /.lbl-cnt -->
-                </div>
-                <!-- /.col -->
-                <div
-                  class="col col-sm-6 col-md-6 no-padding hidden-sm hidden-md"
-                >
-                  <div class="lbl-cnt">
-                    <span class="lbl">Show</span>
-                    <div class="fld inline">
-                      <div
-                        class="
-                          dropdown dropdown-small dropdown-med dropdown-white
-                          inline
-                        "
-                      >
-                        <button
-                          data-toggle="dropdown"
-                          type="button"
-                          class="btn dropdown-toggle"
-                        >
-                          1 <span class="caret"></span>
-                        </button>
-                        <ul role="menu" class="dropdown-menu">
-                          <li role="presentation"><a href="#">1</a></li>
-                          <li role="presentation"><a href="#">2</a></li>
-                          <li role="presentation"><a href="#">3</a></li>
-                          <li role="presentation"><a href="#">4</a></li>
-                          <li role="presentation"><a href="#">5</a></li>
-                          <li role="presentation"><a href="#">6</a></li>
-                          <li role="presentation"><a href="#">7</a></li>
-                          <li role="presentation"><a href="#">8</a></li>
-                          <li role="presentation"><a href="#">9</a></li>
-                          <li role="presentation"><a href="#">10</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <!-- /.fld -->
-                  </div>
-                  <!-- /.lbl-cnt -->
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.col -->
-              <div class="col col-sm-6 col-md-4 col-xs-6 col-lg-4 text-right">
-                <div class="pagination-container">
-                  <ul class="list-inline list-unstyled">
-                    <li class="prev">
-                      <a href="#"><i class="fa fa-angle-left"></i></a>
-                    </li>
-                    <li><a href="#">1</a></li>
-                    <li class="active"><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li class="next">
-                      <a href="#"><i class="fa fa-angle-right"></i></a>
-                    </li>
-                  </ul>
-                  <!-- /.list-inline -->
-                </div>
-                <!-- /.pagination-container -->
-              </div>
-              <!-- /.col -->
-            </div>
-            <!-- /.row -->
-          </div>
           <div class="search-result-container">
             <div id="myTabContent" class="tab-content category-list">
               <div class="tab-pane active" id="grid-container">
@@ -889,14 +772,14 @@
                               <div class="image">
                                 <a href="detail.html">
                                   <img
-                                    src="assets/images/products/p1.jpg"
+                                    :src="product.image"
                                     alt=""
                                   />
-                                  <img
+                                  <!-- <img
                                     src="assets/images/products/p1_hover.jpg"
                                     alt=""
                                     class="hover-image"
-                                  />
+                                  /> -->
                                 </a>
                               </div>
                               <!-- /.image -->
@@ -1170,18 +1053,25 @@
 </template>
 
 
+
+
 <script lang="ts">
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import axios from "@/axios";
-export default {
-  data() {
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+   data() {
     return {
-      products: [],
+      products: null as any,
     };
   },
   mounted() {
-    axios.get("product/").then(({ data }) => {
+    axios.get("product/").then(({ data }: any) => {
       this.products = data;
     });
-  },
-};
+  }
+});
 </script>
